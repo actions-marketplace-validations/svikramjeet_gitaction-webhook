513955a28449e7ae0690a2294cad807a2a41f805
Vikramjeet Singh <vikramjeet@founderandlightning.com>
2022-10-10 15:22:27 +0530

    Update entrypoint.php

--- entrypoint.php
+++ entrypoint.php
@@ -9,7 +9,6 @@ rtrim($fields_string, '&');
 
 $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL, $url);
-curl_setopt($ch,CURLOPT_POST, count($fields));
 curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
 $result = curl_exec($ch);
 curl_close($ch);
